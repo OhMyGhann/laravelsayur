da888f7ac5d0a6fd8dd9f5f812e7f23e17d0c356
OhMyGhan <mynameisghan610@gmai.com>
2024-08-09 20:59:45 +0700

    revisi order

--- resources/views/layouts/shop/order.blade.php
+++ resources/views/layouts/shop/order.blade.php
@@ -16,52 +16,60 @@
     <div class="container-fluid py-5">
         <div class="container py-5">
             <div class="row g-4">
-                @foreach ($orders as $order)
+                @if ($orders->isEmpty())
                     <div class="col-md-12">
-                        <div class="card h-100">
-                            <div class="card-header d-flex justify-content-between align-items-center">
-                                <h5 class="card-title mb-0">Pesanan #{{ $order->order_number }}</h5>
-                                <span
-                                    class="badge
-                                @if ($order->status == 'pending') bg-warning
-                                @elseif($order->status == 'processing') bg-info
-                                @elseif($order->status == 'packed') bg-primary
-                                @elseif($order->status == 'completed') bg-success
-                                @elseif($order->status == 'declined') bg-danger @endif">
-                                    {{ 
-                                        $order->status == 'pending' ? 'Menunggu' : (
-                                        $order->status == 'processing' ? 'Sedang Diproses' : (
-                                        $order->status == 'packed' ? 'Sedang Dikemas' : (
-                                        $order->status == 'completed' ? 'Selesai' : (
-                                        $order->status == 'declined' ? 'Ditolak' : ucfirst($order->status)
-                                    ))))
-                                    }}
-                                </span>
-                            </div>
-                            <div class="card-body">
-                                <h6>Total Harga: Rp {{ number_format($order->total_price, 0, ',', '.') }}</h6>
-                                <p>Tanggal Order: {{ $order->created_at->format('d M Y') }}</p>
-                            </div>
-                            <div class="card-footer">
-                                @if ($order->status == 'pending')
-                                    <p class="mb-0">Tunggu Konfirmasi Admin</p>
-                                @elseif($order->status == 'completed')
-                                    <a href="{{ route('order.pdf.download', $order->id) }}" class="btn btn-success">Download Invoice</a>
-                                @elseif($order->status == 'declined')
-                                    <a href="{{ route('order.pdf.download', $order->id) }}" class="btn btn-secondary">Detail Pembelian</a>
-                                @elseif($order->status == 'processing')
-                                    @if ($order->bukti_tf)
-                                        <!-- Tombol untuk melihat bukti transfer -->
-                                        <a href="{{ asset('storage/' . $order->bukti_tf) }}" class="btn btn-secondary">Check Bukti Transfer</a>
-                                    @else
-                                        <!-- Tombol untuk mengunggah bukti pembayaran -->
-                                        <a href="{{ route('showPaymentPage', ['order_id' => $order->id]) }}" class="btn btn-primary">Menunggu Pembayaran</a>
-                                    @endif
-                                @endif
-                            </div>
+                        <div class="alert alert-warning text-center" role="alert">
+                            Tidak ada order.
                         </div>
                     </div>
-                @endforeach
+                @else
+                    @foreach ($orders as $order)
+                        <div class="col-md-12">
+                            <div class="card h-100">
+                                <div class="card-header d-flex justify-content-between align-items-center">
+                                    <h5 class="card-title mb-0">Pesanan #{{ $order->order_number }}</h5>
+                                    <span
+                                        class="badge
+                                        @if ($order->status == 'pending') bg-warning
+                                        @elseif($order->status == 'processing') bg-info
+                                        @elseif($order->status == 'packed') bg-primary
+                                        @elseif($order->status == 'completed') bg-success
+                                        @elseif($order->status == 'declined') bg-danger @endif">
+                                        {{ 
+                                            $order->status == 'pending' ? 'Menunggu' : (
+                                            $order->status == 'processing' ? 'Sedang Diproses' : (
+                                            $order->status == 'packed' ? 'Sedang Dikemas' : (
+                                            $order->status == 'completed' ? 'Selesai' : (
+                                            $order->status == 'declined' ? 'Ditolak' : ucfirst($order->status)
+                                        ))))
+                                        }}
+                                    </span>
+                                </div>
+                                <div class="card-body">
+                                    <h6>Total Harga: Rp {{ number_format($order->total_price, 0, ',', '.') }}</h6>
+                                    <p>Tanggal Order: {{ $order->created_at->format('d M Y') }}</p>
+                                </div>
+                                <div class="card-footer">
+                                    @if ($order->status == 'pending')
+                                        <p class="mb-0">Tunggu Konfirmasi Admin</p>
+                                    @elseif($order->status == 'completed')
+                                        <a href="{{ route('order.pdf.download', $order->id) }}" class="btn btn-success">Download Invoice</a>
+                                    @elseif($order->status == 'declined')
+                                        <a href="{{ route('order.pdf.download', $order->id) }}" class="btn btn-secondary">Detail Pembelian</a>
+                                    @elseif($order->status == 'processing')
+                                        @if ($order->bukti_tf)
+                                            <!-- Tombol untuk melihat bukti transfer -->
+                                            <a href="{{ asset('storage/' . $order->bukti_tf) }}" class="btn btn-secondary">Check Bukti Transfer</a>
+                                        @else
+                                            <!-- Tombol untuk mengunggah bukti pembayaran -->
+                                            <a href="{{ route('showPaymentPage', ['order_id' => $order->id]) }}" class="btn btn-primary">Menunggu Pembayaran</a>
+                                        @endif
+                                    @endif
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                @endif
             </div>
         </div>
     </div>
